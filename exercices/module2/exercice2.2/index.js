e06d5626202c85c85183f585de02762a2623d7df
IweZix <IweZix@users.noreply.github.com>
2023-10-09 15:00:19 +0200

    bonus

--- exercices/module2/exercice2.2/index.js
+++ exercices/module2/exercice2.2/index.js
@@ -1,5 +1,10 @@
 const compteur = document.querySelector('#compteur');
 const phrase = document.querySelector('#phrase');
+const button = document.querySelector('#button');
+
+const buttonHTML = `
+    <button type="button">reset</button>
+`
 
 let clic = 0;
 
@@ -8,6 +13,14 @@ window.addEventListener('click', () => {
     compteur.innerHTML = clic;
     if (clic > 4 && clic < 9)
         phrase.innerHTML = `Bravo, bel échauffement !`;
-    else if (clic > 9)
+    else if (clic > 9){
         phrase.innerHTML = `Vous êtes passé maître en l'art du clic !`;
+        button.innerHTML = `<button type="button">reset</button>`;
+    }
+})
+
+button.addEventListener('click', () => {
+    clic = -1;
+    compteur.innerHTML = clic
+    phrase.innerHTML = ``;
 })
