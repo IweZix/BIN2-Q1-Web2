91695a3339dd89ac353d12c82ef4198b03ac8f4e
IweZix <IweZix@users.noreply.github.com>
2023-10-09 14:05:35 +0200

    2.1 : HMI : first external script

---
+++ exercices/module2/exercice2.1/index.js
@@ -0,0 +1,6 @@
+function addDateTime(message) {
+    const dateTimeNow = new Date();
+    alert(`${dateTimeNow.toLocaleDateString()} : ${message}`);
+}
+
+alert("This is the best moment to have a look at this website !")
